afca89bddf0195cef85c2d1ef2b610fb69c149bb
Mujahidul Amin <mujahidulamin1108152@gmail.com>
2022-11-25 15:13:49 +0600

    error-page-added

--- src/Pages/Home/Categories/BookingModal/BookingModal.js
+++ src/Pages/Home/Categories/BookingModal/BookingModal.js
@@ -5,22 +5,35 @@ import React from 'react';
 
 //booking modal
 const BookingModal = ({ product }) => {
-    
+
     return (
         <>
-                    <input type="checkbox" id="my-modal" className="modal-toggle" />
-                    <div className="modal">
-                        <div className="modal-box">
-                            <h3 className="font-bold text-lg">{product?.itemName}</h3>
-                            <p className="py-4">{product?.location}</p>
-                            <div className="modal-action">
-                                <label htmlFor="my-modal" className="btn">Yay!</label>
-                            </div>
-                        </div>
-                    </div>
+            <input type="checkbox" id="my-modal" className="modal-toggle" />
+            <div className="modal">
+                <div className="modal-box">
+                <label htmlFor="my-modal" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+                    <h3 className="text-left font-bold text-lg">{product?.itemName}</h3>
+                    <form onSubmit= ""
+                    className='grid grid-cols-1 gap-3 mt-8'>
+                        <input type="text" disabled value= {product?.resalePrice  + ' Taka'} className="input w-full input-bordered" />
+                        <select name='slot' className="select select-bordered w-full">
+                            {/* {
+                                slots.map((slot, id) => <option
+                                    key={id}
+                                    value={slot}
+                                >{slot}</option>)
+                            } */}
+                        </select>
+                        <input defaultValue="" disabled name='name' type="text" placeholder="Your Name" className="input w-full input-bordered" />
+                        <input defaultValue="" disabled name='email' type="email" placeholder="Email Address" className="input w-full input-bordered" />
+                        <input name='phone' type="text" placeholder="Phone Number" className="input w-full input-bordered" />
+                        <input className='w-full btn btn-accent' type="submit" value="Submit" />
+                    </form>
+                </div>
+            </div>
 
-    
-            
+
+
         </>
     );
 };
--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -4,6 +4,7 @@ import Categories from '../../Pages/Home/Categories/Categories';
 import CategoryProducts from '../../Pages/Home/Categories/CategoryProducts';
 import Home from '../../Pages/Home/Home/Home';
 import Login from '../../Pages/Login/Login';
+import ErrorPage from '../../Pages/Shared/ErrorPage/ErrorPage';
 
 
 
@@ -12,6 +13,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
                 path: '/',
